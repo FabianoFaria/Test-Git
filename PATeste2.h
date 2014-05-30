3c81a65aee0219fe2d62d3413755165eea24828e
fabiano_hatori <fabiano@joiss.com>
2014-05-30 13:29:35 -0300

    adicionado arquivo teste

---
+++ PATeste2.h
@@ -0,0 +1,13 @@
+//
+//  PATeste2.h
+//  Git test
+//
+//  Created by JoissDev on 30/05/14.
+//  Copyright (c) 2014 joiss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PATeste2 : NSObject
+
+@end
---
+++ PATeste2.m
@@ -0,0 +1,13 @@
+//
+//  PATeste2.m
+//  Git test
+//
+//  Created by JoissDev on 30/05/14.
+//  Copyright (c) 2014 joiss. All rights reserved.
+//
+
+#import "PATeste2.h"
+
+@implementation PATeste2
+
+@end
